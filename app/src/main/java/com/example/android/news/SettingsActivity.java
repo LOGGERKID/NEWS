964d272d4bdc346509d1b0664ea416c93d7784cf
LOGGERKID <revanthms.06@gmail.com>
2018-08-02 21:21:55 +0530

    settings display preferences  (1.17)

--- app/src/main/java/com/example/android/news/SettingsActivity.java
+++ app/src/main/java/com/example/android/news/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.news;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
     public class SettingsActivity extends AppCompatActivity {
@@ -10,11 +13,30 @@ import android.support.v7.app.AppCompatActivity;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.settings_activity);
         }
-        public static class NewsPreferenceFragment extends PreferenceFragment {
+        public static class NewsPreferenceFragment extends PreferenceFragment
+                implements Preference.OnPreferenceChangeListener {
             @Override
             public void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 addPreferencesFromResource(R.xml.settings_main);
+
+                Preference minNews = findPreference(getString(R.string.settings_min_news_key));
+                bindPreferenceSummaryToValue(minNews);
+            }
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object value) {
+                String stringValue = value.toString();
+                preference.setSummary(stringValue);
+                return true;
+            }
+
+            private void bindPreferenceSummaryToValue(Preference preference) {
+                preference.setOnPreferenceChangeListener(this);
+                SharedPreferences preferences =
+                        PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+                String preferenceString = preferences.getString(preference.getKey(), "");
+                onPreferenceChange(preference, preferenceString);
             }
         }
     }
